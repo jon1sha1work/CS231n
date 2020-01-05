49bee562f055ca4a014392ed489fc898bbbea5bb
JS WORK <jon1sha1work@gmail.com>
2020-01-05 14:44:48 +0200

    bla bla

--- assignment1/cs231n/classifiers/softmax.py
+++ assignment1/cs231n/classifiers/softmax.py
@@ -33,7 +33,7 @@ def softmax_loss_naive(W, X, y, reg):
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
     num_of_train = X.shape[0]
-    for i in num_of_train:
+    for i in xrange(num_of_train):
         scores = W.dot(X[i])
         loss += -log(exp(score[y[i]] / np.exp(scores).sum()))
 
